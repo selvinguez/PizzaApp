18e61c8faaddb4a303abe037e91da6ebf9bccf48
christianrivera <chris.rivera9582@gmail.com>
2021-02-27 23:14:04 -0600

    fixed Products Category

--- src/app/gridMenu/grid-menu.component.ts
+++ src/app/gridMenu/grid-menu.component.ts
@@ -20,7 +20,7 @@ export class GridMenu {
   tiles: Tile[] = [
     {text:"Boxes", img: '../../assets/boxes.png', cols: 1, rows: 1, index:0},
     {text:"Entradas", img: '../../assets/entradas.png', cols: 1, rows: 1, index:1},
-    {text:"Hot Packs", img: '../../assets/hotPacks.png', cols: 1, rows: 1, index:2},
+    {text:"Calzoné", img: '../../assets/hotPacks.png', cols: 1, rows: 1, index:2},
     {text:"Llevar", img: '../../assets/llevar.png', cols: 1, rows: 1, index:3},
     {text:"Pastas", img: '../../assets/pastas.png', cols: 1, rows: 1, index:4},
     {text:"Pizza", img: '../../assets/pizza.png', cols: 1, rows: 1, index:5},
