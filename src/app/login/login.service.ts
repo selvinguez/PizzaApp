454cc7b77598961be6b817436db5458bb30fd06c
selvinguez <42685319+selvinguez@users.noreply.github.com>
2021-03-11 22:08:42 -0600

    Avance Backend

--- src/app/login/login.service.ts
+++ src/app/login/login.service.ts
@@ -1,24 +1,41 @@
+import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Toast } from "ngx-toastr";
 import { ToasterService } from "../toaster.service";
 import { userLogin } from "./users.module";
+import { Observable, Subject } from "rxjs";
+
+
 
 @Injectable()
 export class loginService
 {
-    constructor( private msg: ToasterService){}
+    constructor( private msg: ToasterService,private http: HttpClient){
+        if(USERS.length===0){
+            this.getInfo().subscribe(data =>
+                data.forEach(lol=>USERS.push(lol)))
+            }
 
+    }
+  
+    getInfo(): Observable< userLogin[]>{
+        return this.http.get< userLogin[]>('http://localhost:3000/usuarios');
+    }
     getByInfoId(id: string){
+       
         return USERS.find(c => c.Correo === id);
     }
 
     getByInfoVerificacion(id: string,pass: string){
-        return USERS.find(c => c.Correo === id && c.password ===pass);
+     
+        return USERS.find(c => String(c.Correo) === String(id) && String(c.password) === String(pass));
     }    
     addUsers(data:userLogin ){
         if(!this.getByInfoId(data.Correo) && !(data.Correo === null) &&  !(data.password === null) ){
-            USERS.push(data)
+            USERS.push(data) 
             this.msg.success("Usuario " +data.Correo + " Creado")
+          var lol =  " '{"+ "Correo"+":"+data.Correo+","+"password:"+data.password +", isadmin:"+data.isadmin+"}'"
+            return this.http.post('http://localhost:3000/usuarios',JSON.parse(lol)).subscribe();
         }else if((data.Correo === null) && (data.password ===null)){
 
             this.msg.success("Llene los Campos por favor")
@@ -28,7 +45,10 @@ export class loginService
         }
     }
     getadmin(user:userLogin):boolean{
-        return user.isadmin
+        if(user.isadmin == 1){
+            return true;
+        }
+        return false
     }
 
     getadmin2(){
@@ -36,7 +56,10 @@ export class loginService
     }
 
     setAdmin(user:userLogin){
-         isAdmin = user.isadmin
+        if(user.isadmin ==1){
+            isAdmin = true
+        }else{
+         isAdmin =false }
     }
     getLogged(){
         return isNormalLogged
@@ -49,15 +72,4 @@ export class loginService
 }
 let isAdmin: boolean =false
 let isNormalLogged: boolean = false
-const USERS:userLogin[] = [{
-
-    Correo : "admin",
-    password :"admin",
-    isadmin: true
-    
-},{
-    Correo : "lol",
-    password :"lol",
-    isadmin: false   
-}
-]
+const USERS:userLogin[] =[]
