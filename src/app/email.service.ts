8333fa8737efefc1f204a628a9f77b5324d23a4d
selvinguez <42685319+selvinguez@users.noreply.github.com>
2021-03-14 21:14:34 -0600

    Avance Final

--- src/app/email.service.ts
+++ src/app/email.service.ts
@@ -17,6 +17,11 @@ export class EmailService
       map(res => res))
   }
 
+  sendEmail2(data){
+    return this.http.post('http://localhost:3000/textmail', data).pipe(
+      map(res => res))
+  }
+
   private _errorHandler(error: Response) {
     console.error(error);
     return Observable.throw(error || 'Server Error')
